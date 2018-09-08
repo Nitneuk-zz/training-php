8a0202c73fea72dc8322c566cc94a81849bcc95e
Quentin Barloy <quentin@les-tilleuls.coop>
2018-09-08 20:27:12 +0200

    2 - First class

---
+++ Product.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+class Product
+{
+    public $name;
+
+    public $description;
+
+    public $price;
+}
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Hello World';
+require_once 'Product.php';
+
+$product = new Product();
+$product->name = 'Guitare';
+$product->description = 'Guitare folk';
+$product->price = 124.10;
+
+$html = <<<HTML
+<p>
+    Produit : $product->name<br/>
+    Description : $product->description<br/>
+    Prix : $product->price
+</p>
+HTML;
+
+echo $html;
