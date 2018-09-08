193a417c65b5c782937184c61403c7e1dbfd32b0
Quentin Barloy <quentin@les-tilleuls.coop>
2018-09-09 01:19:50 +0200

    6 - Add twig

--- index.php
+++ index.php
@@ -4,12 +4,15 @@ namespace App;
 
 require __DIR__.'/vendor/autoload.php';
 
+$loader = new \Twig_Loader_Filesystem('templates');
+$twig = new \Twig_Environment($loader);
+
 $product1 = new Instrument('Guitare', 'Guitare folk', 124.10, Instrument::TYPE_GUITARE);
 
 $product2 = new Disc();
 $product2
     ->setName('Electric Ladyland')
-    ->setDescription('Apprends a jouer les meilleurs chanson d\'Hendrix')
+    ->setDescription('Apprends a jouer les meilleurds chansons d\'Hendrix')
     ->setPrice(15.99)
     ->setArtist('Jimi Hendrix')
 ;
@@ -17,17 +20,6 @@ $product2
 $products = [$product1, $product2];
 
 foreach ($products as $product) {
-
-    if ($product instanceof Instrument) {
-        echo '<strong>Instrument :</strong>';
-    } else if ($product instanceof Disc) {
-        echo '<strong>Disc : </strong>';
-    }
-
-    echo $product->format();
-
-    echo $product->getPrice().' €';
-
-    echo '<hr/>';
+    echo $twig->render('product.html.twig', array('product' => $product));
 }
 
